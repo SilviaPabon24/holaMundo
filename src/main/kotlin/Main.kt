c04b7429837bd477c62e6fc4b87ad780a8bba65b
SilviaPabon24 <silvia.juliana24@hotmail.com>
2021-08-01 21:24:19 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("¡Hola Mundo!")
+
+    println("Integrantes:\n" +
+            "Diyani Diaz Araujo\n" +
+            "Aldahir Lain Mendoza Martinez\n" +
+            "Silvia Juliana Pabón Chacón")
+}
